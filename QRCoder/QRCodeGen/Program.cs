ef40720cb9a143d6bd849aaf5ebdf10f0d581744
Zahar <zaharvaskovskij78@gmail.com>
2025-06-15 21:50:53 +0300

    add files

---
+++ QRCoder/QRCodeGen/Program.cs
@@ -0,0 +1,33 @@
+using System.Drawing;
+using System.Drawing.Imaging;
+using QRCoder;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+Bitmap qr = new QrGenerator().GenerateQrCode("https://example.com");
+qr.Save("qrcode.png", ImageFormat.Png);
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
---
+++ QRCoder/QRCodeGen/QrCodeGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System.Drawing;
+
+namespace QRCoder
+{
+    public class QrGenerator
+    {
+        public Bitmap GenerateQrCode(string url)
+        {
+            using var generator = new QRCodeGenerator();
+            QRCodeData data = generator.CreateQrCode(url, QRCodeGenerator.ECCLevel.Q);
+
+            using var qrCode = new QRCoder.QRCode(data);
+            Bitmap qrImage = qrCode.GetGraphic(20);
+            return new Bitmap(qrImage); // Клонируем изображение, т.к. оригинал будет удалён при using
+        }
+    }
+}
